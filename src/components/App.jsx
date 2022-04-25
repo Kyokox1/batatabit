717be8e63ba22fc45381dbb20dd78baf6d09a428
kyoko <kevin.kbr123@gmail.com>
2022-04-25 15:34:01 -0400

    Agregando estilos al header, metodologia BEM.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React from "react";
+
+import "../styles/styles.scss";
 
 function App() {
-
-  return (
-    <>
-      <header className='header' >
-        <img className='header__img' src='/assets/images/batabit-header.svg' alt="header"/>
-        <div>
-          <h1 className='header__title' >La próxima revolución en el intercambio de criptomonedas.</h1>
-          <p className='header__description' >Batabit te ayuda a navegar entre los diferentes precios y tendencias.</p>
-          <button className='button' >
-            <a src="#" className='button__link' >Conoce Nuestros Planes <span className='button__icon' >i</span> </a>
-          </button>
-        </div>
-      </header>
-    </>
-  )
+	return (
+		<>
+			<header className="header">
+				<img
+					className="header__img"
+					src="/assets/images/batabit-header.svg"
+					alt="header"
+				/>
+				<div>
+					<h1 className="header__title">
+						La próxima revolución en el intercambio de criptomonedas.
+					</h1>
+					<p className="header__description">
+						Batabit te ayuda a navegar entre los diferentes precios y
+						tendencias.
+					</p>
+					<button className="button button-header">
+						<a src="#" className="button__link">
+							Conoce Nuestros Planes <span className="button__icon"></span>{" "}
+						</a>
+					</button>
+				</div>
+			</header>
+		</>
+	);
 }
 
-export default App
+export default App;
