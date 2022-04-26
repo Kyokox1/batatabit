930c421b3ada07917e448f7e6f42f720d18d1ae7
kyoko <kevin.kbr123@gmail.com>
2022-04-25 20:53:38 -0400

    Estructurando la primera seccion de Bitcoins, agregando estilos.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,23 @@ import React from "react";
 
 import "../styles/styles.scss";
 
+import arrowUp from "/assets/icons/trending-up.png";
+import arrowDown from "/assets/icons/trending-down.png";
+
 function App() {
+	const bitcoins = {
+		Bitcoin: "$ 1.96",
+		Ethereum: "$ 0.07",
+		Ripple: "$ 2.15",
+		Stellar: "$ 4.96"
+	};
+
+	const bitcoinsArray = Object.entries(bitcoins);
+	console.log(bitcoinsArray);
+
 	return (
 		<>
+			{/* header */}
 			<header className="header">
 				<img
 					className="header__img"
@@ -12,10 +26,10 @@ function App() {
 					alt="header"
 				/>
 				<div>
-					<h1 className="header__title">
+					<h1 className="title header__title">
 						La próxima revolución en el intercambio de criptomonedas.
 					</h1>
-					<p className="header__description">
+					<p className="description header__description">
 						Batabit te ayuda a navegar entre los diferentes precios y
 						tendencias.
 					</p>
@@ -26,6 +40,47 @@ function App() {
 					</button>
 				</div>
 			</header>
+
+			{/* main */}
+
+			<main>
+				<section className="bitcoin">
+					<img className="bitcoin__img" src="/assets/images/bitcoin.svg" />
+					<div className="bitcoin__content">
+						<h2 className="title">
+							Visibilizamos todas las tasas de cambio.
+						</h2>
+						<p className="description bitcoin__description">
+							Traemos informacion en tiempo real de las casas de cambio y
+							las monedas más importantes del mundo.
+						</p>
+					</div>
+					<div className="bitcoin__table">
+						<p>Monedas.</p>
+						<table>
+							{bitcoinsArray.map((el, i) => (
+								<tr key={i}>
+									<td className="bitcoin__table-left-column">{el[0]}</td>
+									<td className="bitcoin__table-rigth-column">
+										{el[1]}
+										<img
+											className="bitcoin__table-arrow"
+											src={i === 1 ? arrowUp : arrowDown}
+											alt="trendingArrow"
+										/>
+									</td>
+								</tr>
+							))}
+						</table>
+						<p className="bitcoin__table-date">
+							<b>Actualizado:</b> 24 Abril 23:45{" "}
+						</p>
+					</div>
+				</section>
+				<section></section>
+				<section></section>
+				<section></section>
+			</main>
 		</>
 	);
 }
