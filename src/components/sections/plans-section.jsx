8c4eca130ba3b42020e4e529d5970c63f1362f00
kyoko <kevin.kbr123@gmail.com>
2022-04-29 16:23:11 -0400

    Agregando estilos al Slide de la seccion Plans.

--- src/components/sections/plans-section.jsx
+++ src/components/sections/plans-section.jsx
@@ -1,21 +1,68 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export const PlansSection = () => {
+	// const slide = useRef(null)
+
+	// const siguiente = ()=>{
+
+	// 	slide.current.style.transition=`3s all ease-out`
+	// 	slide.current.scrollTo({
+	// 		right:500
+	// 	})
+	// }
+
 	return (
 		<section id="plans">
 			<div>
 				<h2 className="title plans__title ">
 					Escoge el plan que mejor se ajusta a ti.
 				</h2>
-				<p className="description plans__description  ">
+				<p className="description plans__description">
 					Cualquier te da acceso completo a nuestra plataforma.
 				</p>
 			</div>
 			<section className="plans__cards">
 				<article className="plans__card">
+					<p className="plans__card-head hidden ">Recomendado</p>
+					<div className="plans__card-body">
+						<h3 className="description plans__card-title ">
+							Pago Mensual
+						</h3>
+						<p className="plans__card-price">
+							<span>$</span> 99
+						</p>
+						<p className="plans__card-description">
+							*Ahorras $129 comparado al plan mensual.
+						</p>
+						<button
+							className="button plans__card-button "
+							onClick={siguiente}
+						>
+							Escoger este <span></span>{" "}
+						</button>
+					</div>
+				</article>
+				<article className="plans__card">
 					<p className="plans__card-head">Recomendado</p>
 					<div className="plans__card-body">
 						<h3 className="description plans__card-title ">Pago Anual</h3>
+						<p className="plans__card-price">
+							<span>$</span> 99
+						</p>
+						<p className="plans__card-description">
+							*Ahorras $129 comparado al plan mensual.
+						</p>
+						<button className="button plans__card-button ">
+							Escoger este <span></span>{" "}
+						</button>
+					</div>
+				</article>
+				<article className="plans__card">
+					<p className="plans__card-head hidden ">Recomendado</p>
+					<div className="plans__card-body">
+						<h3 className="description plans__card-title ">
+							Pago Semanal
+						</h3>
 						<p className="plans__card-price">
 							<span>$</span> 99
 						</p>
